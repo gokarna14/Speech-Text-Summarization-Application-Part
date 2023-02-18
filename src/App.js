12cd157115e9d5246f5f6aee35ba475ba883bac9
Prabas Adhikari <gokarna.adhikari14@gmail.com>
2023-02-18 15:23:07 +0545

    Completed the preliminary front end

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 // import logo from './logo.svg';
 import './App.css';
 import Home from './Home/Home';
+import AnimatedText from 'react-animated-text-content';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 
@@ -9,14 +11,67 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <header classNameName="App-header">
+      <div className="container p-3">
+        <div className="row text-center">
+          <hr />
           <h1>
-            Welcome to Text Summarization !!!
+            <AnimatedText
+              type="words" // animate words or chars
+              animation={{
+                x: '200px',
+                y: '-20px',
+                scale: 1.1,
+                ease: 'ease-in-out',
+              }}
+              animationType="wave"
+              interval={0.06}
+              duration={1}
+              tag="p"
+              className="animated-paragraph"
+              includeWhiteSpaces
+              threshold={0.1}
+              rootMargin="20%"
+            >
+              Welcome to Speech Text Summarization !
+            </AnimatedText>
           </h1>
-          <Home></Home>
-      </header>
-    </div>
+          <hr />
+
+        </div>
+        <div className="row">
+          <div className="col-1 border border-1 border-danger">
+          </div>
+          <div className="col">
+            <Home></Home>
+          </div>
+          <div className="col-1 border border-1 border-danger">
+          </div>
+        </div>
+              <hr />
+        <div className="row text-center">
+          <AnimatedText
+            type="words" // animate words or chars
+            animation={{
+              x: '200px',
+              y: '-20px',
+              scale: 1.1,
+              ease: 'ease-in-out',
+            }}
+            animationType="wave"
+            interval={0.06}
+            duration={1}
+            tag="p"
+            className="animated-paragraph"
+            includeWhiteSpaces
+            threshold={0.1}
+            rootMargin="20%"
+          >
+               © Speech Text Summarization, 2023
+          </AnimatedText> 
+        </div>
+      </div>
+    </header>
   );
 }
 
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,28 +1,31 @@
 import React from 'react'
 import { useState } from 'react'
 import Swal from 'sweetalert2'
+import { settings } from './icons'
 
 
-const Home = ()=>{
+const Home = () => {
 
     // const filter
 
-    const size = {
-        "S":0.1,
-        "M":0.5,
-        "L":0.8,
-    }
 
-    const [text, setText] = useState("")
+    const sizes = ["Small", "Medium", "Large"];
+    
+    const sizesToSendToApi = [0.1, 0.5, 0.8]
+    
+
+    const [text, setText] = useState('')
     const [data, setData] = useState(null);
     const [textId, setTextId] = useState(0)
-    const [selectedSize, setSelectedSize] = useState('S');
+    const [selectedSize, setSelectedSize] = useState('M');
+    const [showSettings, setShowSettings] = useState(false);
+    const [rangeValue, setRangeValue] = useState(2)
 
     const [summary, setSummary] = useState("")
 
 
     async function getData(url, setFunction, method = "GET", body = null) {
-        if (method === "GET"){
+        if (method === "GET") {
             await fetch(url,
                 {
                     method: method,
@@ -31,14 +34,14 @@ const Home = ()=>{
                     // },
                     // body: method = "GET" ? "" :  JSON.stringify(body)
                 })
-            .then(response => response.json())  
-            .then(json => {
-                // console.log(json);
-                setFunction(json);
-                // document.getElementById("demo").innerHTML = JSON.stringify(json)
-            })
+                .then(response => response.json())
+                .then(json => {
+                    // console.log(json);
+                    setFunction(json);
+                    // document.getElementById("demo").innerHTML = JSON.stringify(json)
+                })
         }
-        else{
+        else {
             await fetch(url,
                 {
                     method: method,
@@ -47,25 +50,25 @@ const Home = ()=>{
                     },
                     body: JSON.stringify(body)
                 })
-            .then(response => response.json())  
-            .then(json => {
-                // console.log(json);
-                setFunction(json);
-                // document.getElementById("demo").innerHTML = JSON.stringify(json)
-            })
+                .then(response => response.json())
+                .then(json => {
+                    // console.log(json);
+                    setFunction(json);
+                    // document.getElementById("demo").innerHTML = JSON.stringify(json)
+                })
         }
     }
 
 
-    const generateSummaryFromModel = (text)=>{
+    const generateSummaryFromModel = (text) => {
         return text;
     }
 
-    async function generateSummary(){
+    async function generateSummary() {
 
-        setText(toString(text)) 
-        console.log(text);
-        
+        setText(text)
+        // console.log(text);
+
         // post text
         await fetch(
             "http://127.0.0.1:5000/text",
@@ -79,16 +82,16 @@ const Home = ()=>{
                 })
             }
         )
-        
+
         //Determining the latest text id
         let text_id = await fetch("http://127.0.0.1:5000/text")
-        .then(response => response.json())  
-        .then(json => {
-            // setTextId(json["MAX(text_id)"]);
-            // new Promise(()=>setTextId(json["MAX(text_id)"]))
-            return json["MAX(text_id)"]
-        })
-        
+            .then(response => response.json())
+            .then(json => {
+                // setTextId(json["MAX(text_id)"]);
+                // new Promise(()=>setTextId(json["MAX(text_id)"]))
+                return json["MAX(text_id)"]
+            })
+
 
         // console.log(text_id);
 
@@ -101,16 +104,16 @@ const Home = ()=>{
                 },
                 body: JSON.stringify({
                     "text": text,
-                    "compression_ratio":  size[selectedSize]
+                    "compression_ratio": sizesToSendToApi[rangeValue]
                 })
             }
         )
-        .then(response => response.json())  
-        .then(json => {
-            // setTextId(json["MAX(text_id)"]);
-            // new Promise(()=>setTextId(json["MAX(text_id)"]))
-            return json["summary"]["summary"]
-        })
+            .then(response => response.json())
+            .then(json => {
+                // setTextId(json["MAX(text_id)"]);
+                // new Promise(()=>setTextId(json["MAX(text_id)"]))
+                return json["summary"]["summary"]
+            })
 
 
         setSummary(summary_);
@@ -124,44 +127,44 @@ const Home = ()=>{
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    
-                        "summary" : summary_,
-                        "compression_ratio" : size[selectedSize],
-                        "text_id" : text_id
+
+                    "summary": summary_,
+                    "compression_ratio": sizesToSendToApi[rangeValue],
+                    "text_id": text_id
                 })
             }
         )
 
-        
+
 
     }
-    
-    const summarizationClicked = async ()=>{
+
+    const summarizationClicked = async () => {
         // console.log(text);
 
-        if (text.length < 50){
+        if (text.length < 50) {
             Swal.fire({
                 icon: 'error',
                 title: 'Oops...',
                 text: 'The input text is too short to summarize!!! Try again with longer length (> 50 characters)',
                 // footer: '<a href="">Why do I have this issue?</a>'
-              })
+            })
         }
-        else{
+        else {
             Swal.fire({
                 title: 'Do you want to generate the summary?',
                 showDenyButton: true,
                 showCancelButton: true,
                 confirmButtonText: 'YES',
                 denyButtonText: `NO`,
-              }).then((result) => {
+            }).then((result) => {
                 /* Read more about isConfirmed, isDenied below */
                 if (result.isConfirmed) {
-                    generateSummary(text) 
+                    generateSummary(text)
                 } else if (result.isDenied) {
-                  Swal.fire('OKAY !!', 'No summary is generated please try again !', 'info')
+                    Swal.fire('OKAY !!', 'No summary is generated please try again !', 'info')
                 }
-              })
+            })
         }
 
         // generateSummary(text) 
@@ -171,7 +174,7 @@ const Home = ()=>{
         //     "summary" : "The Summary from ReactJS",
         //     "compression_ratio" : "0.5",
         //     "text_id" : 13
-        
+
         // }
 
 
@@ -179,67 +182,81 @@ const Home = ()=>{
     }
 
     const handleChange = (event) => {
-        setSelectedSize(event.target.value);
-      }
+        if (event.target.value !== "") {
+            setSelectedSize(event.target.value);
+        }
+    }
 
 
     return (
-        <>
-            <hr />
+        <div>
+            <div class="p-3 mb-2">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-6 my-2 p-2">
+                            <div className="input-group input-group-lg">
+                                <textarea type="text" className="form-control border rounded border-primary" aria-label="Large" aria-describedby="inputGroup-sizing-sm"
+                                    placeholder='Type your text here ...'
+                                    rows="15"
+                                    cols="50"
+                                    value={text}
 
-            <div className="SizeDropdown">
-                <label>
-                    How long do you want your summarization to be ?
-                    <br />
-                    <select value={selectedSize} onChange={handleChange}>
-                    <option value="S">Small</option>
-                    <option value="M">Medium</option>
-                    <option value="L">Long</option>
-                    </select>
-                </label>
-            </div>
-            
-            <hr />
-                <div className="input-group-prepend">
-                    <span className="input-group-text" id="inputGroup-sizing-lg">Input Your Text Here</span> 
+                                    onChange={(e) => {
+                                        setText(e.target.value);
+                                        setSummary("");
+                                    }}
+                                />
+                            </div>
+                            <div className="row mx-auto p-2">
+                                <div className="col btn btn-warning"
+                                    onClick={() => {summarizationClicked()}}
+                                >
+                                    {settings} Summarize {settings}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-6 my-2 p-2">
+                            <div>
+                                <div className="container" style={{ fontSize: '20px' }}>
+                                    <div className="row">
+                                        <div className="float-start col">
+                                            <span className='float-start'>Small</span>
+                                        </div>
+                                        <div className="col center text-center">
+                                            <span>Medium</span>
+                                        </div>
+                                        <div className="col">
+                                            <span className='float-end'>Large</span>
+                                        </div>
+                                    </div>
+                                    <div className="row p-1">
+                                        <input type="range" class="form-range" id="customRange1" min={0} max={2} step={1} onChange={(e) => { 
+                                            setRangeValue(parseInt(e.target.value));
+                                         }} />
+                                         {/* {rangeValue} */}
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="input-group input-group-lg">
+                                <textarea type="text" className="form-control border rounded border-success" aria-label="Large" aria-describedby="inputGroup-sizing-sm"
+                                    placeholder='Your summary will appear here ...'
+                                    value={summary === "" || text == "" ? "" : summary}
+                                    disabled
+                                    rows="12"
+                                    cols="50"
+                                    // value={"Your text to summarize here ... "}
+
+                                    onChange={(e) => {
+                                        // TODO
+                                    }}
+                                />
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-            <hr />
-
-            <div className='niceCenter'>
-                <div className="input-group input-group-lg">
-                    <br />
-                    <textarea type="text" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm"
-                        rows="10" 
-                        cols="70"
-                        // value={"Your text to summarize here ... "}
-
-                    onChange={(e)=>{
-                        setText(e.target.value)
-                    }}
-
-                    />
-                </div>
-
-                
-
-
-                
             </div>
-            
-            <hr />
-            <button
-                onClick={summarizationClicked}
-            >Summarize !</button>
-            <hr />
-            
-            {
-                (summary !== "") && <>
-                    {summary}
-                </>
-            }
-
-        </>
+        </div>
     )
 
 }
@@ -248,3 +265,11 @@ export default Home;
 
 
 
+
+
+
+// In Sanskrit, Maha means great or big, and Shivaratri means a night dedicated to Shiva. According to the Rudra Samhita, the wedding of Shiva and Parvati took place in the Himalayas. On that day, Parvati transformed herself into Chandraghanta with golden skin and ten arms, and they got married in their beautiful divine forms at Triyuginarayan village in Rudraprayag, India. So, their marriage is celebrated as Mahashivaratri every year.
+
+// Mahashivaratri is an important festival that is widely celebrated in both Nepal and India, though with different perspectives and processes. Some fast for the entire day and perform vedic or tantric worship of Shiva, while others practise meditative yoga. People also perform the Rudra Abhishek, a special type of puja to please Lord Shiva and seek his blessings. The rituals are carried out throughout the day or in different muhurtas (ancient measurement units for time). Though the daytime rituals differ, at night people generally stay awake doing bhajan, kirtans, meditation, sadhana, upasana, etc.
+
+// “Mahashivaratri is a magnificent occasion for the followers of Lord Shiva to praise him and seek his blessings. In fact, for devotees of Lord Shiva, nothing is more important than fasting on this day,” said Narayan Bhatt, a priest at Pashupatinath Temple. A common ritual a lot of people follow is to take a bath in a river early in the morning or in warm water with sesame seeds at their homes in order to clean themselves. “Devotees can fast for 24 hours without eating or drinking, but they can also fast by drinking water and eating sattvic food (unprocessed food with yogic qualities to increase energy),” added the priest.
